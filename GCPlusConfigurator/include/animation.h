f3c18930d062894a739fd2efbb6e3a808573d9ca
Aurelio <aurelio.mannara@gmail.com>
2019-11-06 02:30:42 +0100

    Animation class

--- GCPlusConfigurator/include/animation.h
+++ GCPlusConfigurator/include/animation.h
@@ -9,18 +9,20 @@ class Animation {
     private:
         class Step {
             public:
-                u64 t;
+                u64 time;
+                u64 duration;
                 T val;
         };
 
         u64 timer; //milliseconds
-        u64 duration;
+        u64 totDuration;
         vector<Step> steps;
         bool playing;
 
+        void findStep(u64 time);
     public:
         Animation();
         ~Animation();
 
-        void addStep(u64 t, T val);
+        void addStep(u64 duration, T val);
 };
--- GCPlusConfigurator/source/animation.cpp
+++ GCPlusConfigurator/source/animation.cpp
@@ -2,10 +2,43 @@
 
 Animation::Animation() {
     timer = 0;
-    duration = 0;
+    totDuration = 0;
     playing = false;
 }
 
 Animation::~Animation() {
-    
+    steps.clear();
+}
+
+void Animation::addStep(u64 duration, T val) {
+    Step s;
+    if (steps.empty()) {
+        //steps vector is empty. Start from zero
+        s.time = 0;
+    } else {
+        //steps vector is not empty. Compute start time for this step
+        Step back = steps.back();
+        s.time = back.time + back.duration;
+    }
+    s.duration = duration;
+    s.val = val;
+    steps.push_back(s);
+
+    //Increment total duration
+    totDuration += duration;
+}
+
+size_t Animation::findStep(u64 time) {
+    if (time >= duration)
+        return 0;
+
+    size_t left = 0;
+    size_t right = steps.size() - 1;
+    size_t center;
+
+    while(left < right) {
+        center = (right - left) / 2;
+        s = steps[center];
+
+    }
 }
